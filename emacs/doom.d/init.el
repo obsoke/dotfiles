eb3c85cf8037285d88a7d148bfabca1c24663f06
Dale Karp <dale@dale.io>
2018-05-28 21:07:57 -0400

    Lots of updates
    compton: remove shadows
    polybar: update, stolen from some unixporn config
    emacs: update
    and other stuff

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -26,9 +26,6 @@
         +childframe)     ; uses childframes for popups (Emacs 26+ only)
 
        :ui
-       (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
-        +defaults)       ; default popup rules
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-modeline     ; a snazzy Atom-inspired mode-line
@@ -37,36 +34,42 @@
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        nav-flash         ; blink the current line after jumping
        neotree           ; a project drawer, like NERDTree for vim
+       (popup            ; tame sudden yet inevitable temporary windows
+        +all             ; catch all popups that start with an asterix
+        +defaults)       ; default popup rules
       ;tabbar            ; FIXME an (incomplete) tab bar for Emacs
-      ;unicode           ; extended unicode support for various languages
+       unicode           ; extended unicode support for various languages
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
 
-       :tools
+       :emacs
        dired             ; making dired pretty [functional]
+       ediff             ; comparing files in Emacs
+       electric-indent   ; smarter, keyword-based electric-indent
+      ;eshell            ; a consistent, cross-platform shell (WIP)
+      ;imenu             ; an imenu sidebar and searchable code index
+      ;term              ; terminals in Emacs
+
+       :tools
        editorconfig      ; let someone else argue about tabs vs spaces
       ;ein               ; tame Jupyter notebooks with emacs
-       electric-indent   ; smarter, keyword-based electric-indent
-       eshell            ; a consistent, cross-platform shell (WIP)
-       gist              ; interacting with github gists
-       imenu             ; an imenu sidebar and searchable code index
+      ;gist              ; interacting with github gists
       ;macos             ; MacOS-specific commands
-       make              ; run make tasks from Emacs
+      ;make              ; run make tasks from Emacs
        magit             ;
       ;password-store    ; password manager for nerds
-       pdf               ; pdf enhancements
-       prodigy           ; Managing external services
-      ;rgb               ; creating color strings
-       rotate-text       ; cycle region at point between text candidates
-       term              ; terminals in Emacs
+      ;pdf               ; pdf enhancements
+      ;prodigy           ; FIXME managing external services & code builders
+       rgb               ; creating color strings
+      ;rotate-text       ; cycle region at point between text candidates
       ;tmux              ; an API for interacting with tmux
-       upload            ; map local to remote projects via ssh/ftp
+      ;upload            ; map local to remote projects via ssh/ftp
 
        :lang
-       assembly          ; assembly for fun or debugging
-       cc                ; C/C++/Obj-C madness
+      ;assembly          ; assembly for fun or debugging
+      ;cc                ; C/C++/Obj-C madness
       ;crystal           ; ruby at the speed of c
-       clojure           ; java with a lisp
+      ;clojure           ; java with a lisp
       ;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
       ;erlang            ; an elegant language for a more civilized age
@@ -82,7 +85,7 @@
       ;julia             ; a better, faster MATLAB
        latex             ; writing papers in Emacs has never been so fun
       ;ledger            ; an accounting system in Emacs
-       lua               ; one-based indices? one-based indices
+      ;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
       ;nim               ; python + lisp at the speed of c
       ;nix               ; I hereby declare "nix geht mehr!"
@@ -92,20 +95,19 @@
         +babel           ; running code in org
         +capture         ; org-capture in and outside of Emacs
         +export          ; Exporting org to whatever you want
-        +present         ; Emacs for presentations
-        +publish)        ; Emacs+Org as a static site generator
+        +present)        ; Emacs for presentations
       ;perl              ; write code no one else can comprehend
       ;php               ; perl's insecure younger brother
       ;plantuml          ; diagrams for confusing people more
       ;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
-       rest              ; Emacs as a REST client
+      ;python            ; beautiful is better than ugly
+      ;rest              ; Emacs as a REST client
       ;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
       ;scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
+      ;solidity          ; do you need a blockchain? No.
       ;swift             ; who asked for emoji variables?
-       typescript        ; javascript, but better
        web               ; the tubes
 
        ;; Applications are complex and opinionated modules that transform Emacs
@@ -121,12 +123,13 @@
       ; +langtool)       ; a proofreader (grammar/style check) for Emacs
 
        :collab
+      ;floobits          ; peer programming for a price
       ;impatient-mode    ; show off code over HTTP
 
-      :config
-      ;; The default module set reasonable defaults for Emacs. It also provides
-      ;; a Spacemacs-inspired keybinding scheme, a custom yasnippet library,
-      ;; and additional ex commands for evil-mode. Use it as a reference for
-      ;; your own modules.
+       :config
+       ;; The default module set reasonable defaults for Emacs. It also provides
+       ;; a Spacemacs-inspired keybinding scheme, a custom yasnippet library,
+       ;; and additional ex commands for evil-mode. Use it as a reference for
+       ;; your own modules.
        (default +bindings +snippets +evil-commands))
 
