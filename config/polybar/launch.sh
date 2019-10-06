4c284131e2fede9208457344ce45a89f83f2495e
Dale Karp <dale@dale.io>
2019-10-06 19:31:04 -0400

    polybar: update theme

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
+## Add this to your wm startup file.
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars
-polybar main &
-polybar secondary &
+# Launch
+MONITOR=HDMI-0 polybar -c ~/.config/polybar/config.ini --reload main &
+MONITOR=DVI-I-1 polybar -c ~/.config/polybar/config.ini --reload two &
