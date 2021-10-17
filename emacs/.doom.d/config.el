6d59f5e9dcebdc2382eeddcc783b946256e3ec13
Dale Karp <dale@dale.io>
2021-10-17 15:29:23 -0400

    emacs: config cleanup!

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,9 +1,3 @@
-(setq org-roam-directory "/home/dale/documents/Dropbox/org/roam"
-      org-roam-completion-anywhere t)
-
-
-
-
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
